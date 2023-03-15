4242ba3b43d013f23cef737e14e674c581cc7d59
s3rgs3m <sergsem@gmail.com>
2023-03-15 12:21:40 +0300

    Final

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://github.com/s3rgs3m/Itog1
+Console.Clear();
+Console.WriteLine("Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.");
+String[] Arr = {"123","тест","три","-546",":-О","последний", "22"};
+Console.WriteLine("Начальный массив: 123, тест, три, -546, :-О, последний, 22");
+String[] Result=new String[Arr.Length];
+int cnt = 0;
+for (int i=0; i<Arr.Length;i++)
+    if (Arr[i].Length<=3){
+        Result[cnt] = Arr[i];
+        cnt=cnt+1;
+    }
+
+Console.Write("Получившийся массив: ");
+foreach (var item in Result)
+{
+    if (item is not null) Console.Write(item + " ") ;    
+}
+
